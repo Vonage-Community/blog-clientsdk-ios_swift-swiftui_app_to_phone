ae95688aafdcc62bd2c0fcea4a102981a300f778
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2023-05-19 16:04:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# SwiftUI making a phone call
+
+This is a SwiftUI project that uses the [Client SDK](https://developer.vonage.com/vonage-client-sdk/overview) to make calls to a specified phone number.
+
+## Welcome to Vonage
+
+If you're new to Vonage, you can [sign up for a Vonage API account](https://dashboard.nexmo.com/sign-up?utm_source=DEV_REL&utm_medium=github&utm_campaign=) and get some free credit to get you started.
+
+## Running the project
+
+Clone the project to your computer, using the terminal install the dependencies using [Cocoapods](https://cocoapods.org) by running `pod install`
+
+
+## Getting Help
+
+We love to hear from you so if you have questions, comments or find a bug in the project, let us know! You can either:
+
+* Open an issue on this repository
+* Tweet at us! We're [@VonageDev on Twitter](https://twitter.com/VonageDev)
+* Or [join the Vonage Developer Community Slack](https://developer.vonage.com/community/slack)
+
+## Further Reading
+
+* Check out the Developer Documentation at <https://developer.vonage.com>
---
+++ SwiftUICall/ContentView.swift
@@ -0,0 +1,144 @@
+//
+//  ContentView.swift
+//  SwiftUICall
+//
+//  Created by Abdulhakim Ajetunmobi on 30/10/2020.
+//
+
+import SwiftUI
+import AVFoundation
+import VonageClientSDKVoice
+
+struct ContentView: View {
+    @ObservedObject var callModel = CallModel()
+    
+    var body: some View {
+        VStack {
+            Text(callModel.status)
+            
+            if self.callModel.status == "Connected" {
+                TextField("Enter a phone number", text: $callModel.number)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .multilineTextAlignment(.center)
+                    .keyboardType(.numberPad)
+                    .disabled(self.callModel.isCalling)
+                    .padding(20)
+                
+                if !self.callModel.isCalling {
+                    Button(action: { self.callModel.callNumber() }) {
+                        HStack(spacing: 10) {
+                            Image(systemName: "phone")
+                            Text("Call")
+                        }
+                    }
+                }
+                
+                if self.callModel.isCalling {
+                    Button(action: { self.callModel.endCall() }) {
+                        HStack(spacing: 10) {
+                            Image(systemName: "phone")
+                            Text("End Call")
+                        }.foregroundColor(Color.red)
+                    }
+                }
+            }
+        }
+        .animation(.default)
+        .onAppear(perform: self.callModel.setup)
+    }
+}
+
+final class CallModel: NSObject, ObservableObject, VGVoiceClientDelegate {
+    
+    @Published var status: String = ""
+    @Published var isCalling: Bool = false
+    let client = VGVoiceClient()
+    var number: String = ""
+    
+    private var callId: String?
+    private let audioSession = AVAudioSession.sharedInstance()
+    
+    func setup() {
+        initializeClient()
+        requestPermissionsIfNeeded()
+        loginIfNeeded()
+    }
+    
+    func initializeClient() {
+        let config = VGClientConfig(region: .US)
+        client.setConfig(config)
+        client.delegate = self
+    }
+    
+    func requestPermissionsIfNeeded() {
+        if audioSession.recordPermission != .granted {
+            audioSession.requestRecordPermission { (isGranted) in
+                print("Microphone permissions \(isGranted)")
+            }
+        }
+    }
+    
+    func updateStatus(_ text: String) {
+        DispatchQueue.main.async {
+            self.status = text
+        }
+    }
+    
+    func resetState() {
+        DispatchQueue.main.async {
+            self.callId = nil
+            self.isCalling = false
+            self.number = ""
+        }
+    }
+    
+    func loginIfNeeded() {
+        guard status != "Connected" else { return }
+        client.createSession("ALICE_JWT") { error, sessionId in
+            if let error {
+                self.updateStatus(error.localizedDescription)
+            } else {
+                self.updateStatus("Connected")
+            }
+        }
+    }
+    
+    func callNumber() {
+        self.isCalling = true
+        client.serverCall(["to": number]) { error, callId in
+            if error == nil {
+                self.callId = callId
+            }
+        }
+    }
+    
+    func endCall() {
+        client.hangup(callId!) { error in
+            if error == nil {
+                self.resetState()
+            }
+        }
+    }
+    
+    func voiceClient(_ client: VGVoiceClient, didReceiveHangupForCall callId: VGCallId, withQuality callQuality: VGRTCQuality, reason: VGHangupReason) {
+        self.resetState()
+    }
+    
+    func client(_ client: VGBaseClient, didReceiveSessionErrorWith reason: VGSessionErrorReason) {
+        let reasonString: String!
+        
+        switch reason {
+        case .tokenExpired:
+            reasonString = "Expired Token"
+        case .pingTimeout, .transportClosed:
+            reasonString = "Network Error"
+        default:
+            reasonString = "Unknown"
+        }
+        
+        status = reasonString
+    }
+    
+    func voiceClient(_ client: VGVoiceClient, didReceiveInviteForCall callId: VGCallId, from caller: String, with type: VGVoiceChannelType) {}
+    func voiceClient(_ client: VGVoiceClient, didReceiveInviteCancelForCall callId: VGCallId, with reason: VGVoiceInviteCancelReason) {}
+}
